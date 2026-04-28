196d8f6ee9d5eaf416a29a970a5567567797a951
allan paris <allanparis8@gmail.com>
2026-04-28 11:14:55 +0200

    initialise database

---
+++ db/schema.sql
@@ -0,0 +1,50 @@
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(20) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(100) NOT NULL,
+    refresh_token TEXT DEFAULT NULL,
+    is_admin BOOLEAN DEFAULT FALSE,
+    is_online BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE posts (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    content_post TEXT NOT NULL,
+    image_url VARCHAR(255),
+    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    status VARCHAR(20) DEFAULT 'active',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE comments (
+    id SERIAL PRIMARY KEY,
+    content TEXT NOT NULL,
+    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    post_id INTEGER REFERENCES posts(id) ON DELETE CASCADE,
+    status VARCHAR(20) DEFAULT 'active',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE likes (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    post_id INTEGER REFERENCES posts(id) ON DELETE CASCADE,
+    is_like BOOLEAN NOT NULL, 
+    UNIQUE(user_id, post_id) -- Empêche un utilisateur de liker plusieurs fois le même post
+);
+
+CREATE TABLE tags (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(27) NOT NULL UNIQUE 
+);
+
+CREATE TABLE post_tags (
+    post_id INTEGER REFERENCES posts(id) ON DELETE CASCADE,
+    tag_id INTEGER REFERENCES tags(id) ON DELETE CASCADE,
+    PRIMARY KEY (post_id, tag_id)
+);
---
+++ dockerfile
@@ -0,0 +1,21 @@
+FROM golang:alpine
+
+# Installation des dépendances système pour Go et Postgres
+RUN apk add --no-cache gcc musl-dev
+
+WORKDIR /app
+
+# Copie des fichiers de modules
+COPY go.mod ./
+
+RUN go mod download
+
+# Copie du reste du code
+COPY . .
+
+# Compilation
+RUN go build -o main .
+
+EXPOSE 8080
+
+CMD ["./main"]
